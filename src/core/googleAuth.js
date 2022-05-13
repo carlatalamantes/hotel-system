13802c261101650061980e50242ad41cc4b27396
Carla Talamantes <carlatalamantes@hotmail.com>
2022-05-12 20:22:29 -0500

    Google auth fix

--- src/core/googleAuth.js
+++ src/core/googleAuth.js
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/google/callback",
+      callbackURL:
+        "https://apihotelsystemteamdinamita.herokuapp.com/google/callback",
       passReqToCallback: true,
       usernameField: "email",
     },
